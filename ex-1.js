b882e7f031579f15bc8479c9dd90840d2a7303d0
FSD7-Boy <boyzasad01@gmail.com>
2024-09-30 15:02:28 +0700

    feat:assignment_4

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+let isAllow = true;
 
 // Start coding here
+let age = 18;
+
+isOver18 = age >= 18;
+hasCriminalBlacklist = false;
+
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,33 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
+let lastMonthPaidMoreThan4000 ;
 let isWeekday;
 let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
 
 let hasPromotion;
+
+// John information
+let lastMonthBought = 4001;
+let boughtDay = "Friday";
+let boughtITCategory = false;
+let hasDiscountEvent = true;
+let memberCard = "Gold";
+
+// Reassign John
+lastMonthPaidMoreThan4000 = lastMonthBought > 4000;
+isWeekday = /Monday|Tuesday|Wednesday|Thursday|Friday/.test(boughtDay);
+hasBoughtProductFromITCategory = boughtITCategory;
+hasAttendedDiscountEvent = hasDiscountEvent;
+isPlatinum = memberCard === "Platinum";
+
+hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    hasBoughtProductFromITCategory &&
+    hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
